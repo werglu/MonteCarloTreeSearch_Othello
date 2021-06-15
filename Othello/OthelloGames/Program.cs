b81d6b1d0ead0a47e3bb08dfe0687ac7ea7776ed
moskala <moskal.ala@gmail.com>
2021-06-15 13:51:31 +0200

    Add project for tests

---
+++ Othello/OthelloGames/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Othello;
+
+namespace OthelloGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
